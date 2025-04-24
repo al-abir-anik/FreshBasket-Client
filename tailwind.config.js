96ce746bb78d90615c4d46b86030ad01d431085a
Al Abir Anik <alabiranik56@gmail.com>
2025-04-24 18:28:29 +0600

    Dark mode Added

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,8 +9,7 @@ export default {
     },
   },
   plugins: [require("daisyui")],
-  // daisyui: {
-  //   themes: ["light", "dark"],
-  //   darkTheme: "dark",
-  // }
+  daisyui: {
+    themes: ["light", "dark"], 
+  },
 };
